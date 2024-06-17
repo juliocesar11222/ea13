7b65adfd8dd9ea1129c023458639e694ee300f7c
julio cesar <202213857h@utea.edu.pe>
2024-06-17 09:18:35 -0500

    varibles

---
+++ Semana3/Variables.java
@@ -0,0 +1,12 @@
+package Semana3;
+
+public class Variables {
+    public static void main(String[] args) {
+       int  numero1;
+       numero1 = 10;
+       System.out.println("el valor actual es :" + numero1);
+
+       numero1 = 20;
+       System.out.println("y ahora es :" + numero1);
+    }
+}
